be7b16708336e70b46648277fb6cb39054290c8b
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2019-05-13 10:09:13 +0900

    fix layout

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-import fwlogo from "./FREEWORKS_ww.png"
-import "../styles/AppBar.scss"
+import logo from "./FREEWORKS.png"
+import "./styles/Header.scss"
 
 const AppBar = () => {
   const linkStyles = {
@@ -12,15 +12,15 @@ const AppBar = () => {
   return (
     <div className="appbar">
       <div className="innerApp">
-    <Link to="/"><img src={fwlogo} alt="logo" /></Link>
-    <ul>
-      <Link style={linkStyles} to="/service"><li>Service</li></Link>
-      <Link style={linkStyles} to="/company"><li>Company</li></Link>
-      <Link style={linkStyles} to="/vision"><li>Vision</li></Link>
-      <Link style={linkStyles} to="/design"><li>Design</li></Link>
-    </ul>
+        <Link to="/"><img src={logo} alt="logo" /></Link>
+        <ul>
+          <Link style={linkStyles} to="/service"><li>Service</li></Link>
+          <Link style={linkStyles} to="/company"><li>Company</li></Link>
+          <Link style={linkStyles} to="/vision"><li>Vision</li></Link>
+          <Link style={linkStyles} to="/design"><li>Design</li></Link>
+        </ul>
+      </div>
     </div>
-  </div>
   )
 }
 export default AppBar;
