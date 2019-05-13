8fc5a43d8bcdc7d5a379dc1963a5dbd599c338dd
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2019-05-13 11:56:07 +0900

    add image

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,38 +3,27 @@ import { Link } from "react-router-dom"
 
 import AppBar from "../components/AppBar.jsx"
 import "./styles/home.scss"
+import topimage from "../components/topimage.png"
 
 export default () => {
   return(
     <React.Fragment>
       <AppBar />
       <div className="HomeApp">
+        <img src={topimage} alt="topimage"/>
         <div className="content">
-          <div className="left">
-            <Link><p>Partnerとして働く</p></Link>
-          </div>
-          <div className="right">
-            <Link><p>法人Partnerとして依頼する</p></Link>
-
-          </div>
+          <Link>
+            <div className="left a1">
+              <p>個人Partnerとして<br/>仕事を受ける</p>
+            </div>
+          </Link>
+          <Link>
+            <div className="right a2">
+              <p>法人Partnerとして<br/>仕事を依頼する</p>
+            </div>
+          </Link>
         </div>
-        <div className="content">
-          <div className="left">
-            hello
-          </div>
-          <div className="right">
-            hello
-          </div>
-        </div>
-
-        <div className="content">
-          <div className="left">
-          </div>
-          <div className="right">
-            
-          </div>
-        </div>
-
+        <div style={{height:100}} />
       </div>
     </React.Fragment>
   )
